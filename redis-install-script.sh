a8c5e36b268dd4b8d21cb9d02bacea2ab4156782
saxenap <saxep01@gmail.com>
2012-06-07 21:08:44 -0400

    first commit

---
+++ redis-install-script.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+# From here: http://www.codingsteps.com/install-redis-2-6-on-amazon-ec2-linux-ami-or-centos/
+# Thanks to https://raw.github.com/gist/2776679/b4f5f5ff85bddfa9e07664de4e8ccf0e115e7b83/install-redis.sh
+###############################################
+# To use: 
+# wget 
+# chmod 777 redis-install-script.sh
+# ./redis-install-script.sh
+###############################################
+echo "*****************************************"
+echo " 1. Prerequisites: Install updates, set time zones, install GCC and make"
+echo "*****************************************"
+#sudo yum -y update
+#sudo ln -sf /usr/share/zoneinfo/America/Indianapolis \ /etc/localtime
+#sudo yum -y install gcc gcc-c++ make 
+echo "*****************************************"
+echo " 2. Download, Untar and Make Redis 2.6"
+echo "*****************************************"
+cd /usr/local/src
+sudo wget http://redis.googlecode.com/files/redis-2.6.0-rc3.tar.gz
+sudo tar xzf redis-2.6.0-rc3.tar.gz
+sudo rm redis-2.6.0-rc3.tar.gz -f
+cd redis-2.6.0-rc3
+sudo make
+echo "*****************************************"
+echo " 3. Create Directories and Copy Redis Files"
+echo "*****************************************"
+sudo mkdir /etc/redis /var/lib/redis
+sudo cp src/redis-server src/redis-cli /usr/local/bin
+echo "*****************************************"
+echo " 4. Configure Redis.Conf"
+echo "*****************************************"
+echo " Edit redis.conf as follows:"
+echo " 1: ... daemonize yes"
+echo " 2: ... bind 127.0.0.1"
+echo " 3: ... dir /var/lib/redis"
+echo " 4: ... loglevel notice"
+echo " 5: ... logfile /var/log/redis.log"
+echo "*****************************************"
+sudo sed -e "s/^daemonize no$/daemonize yes/" -e "s/^# bind 127.0.0.1$/bind 127.0.0.1/" -e "s/^dir \.\//dir \/var\/lib\/redis\//" -e "s/^loglevel verbose$/loglevel notice/" -e "s/^logfile stdout$/logfile \/var\/log\/redis.log/" redis.conf > /etc/redis/redis.conf
+echo "*****************************************"
+echo " 5. Download init Script"
+echo "*****************************************"
+wget https://raw.github.com/gist/2777433/e64b4b8ad7d38676f7e7f70b24f3f004b4f10b74/redis-server
+echo "*****************************************"
+echo " 6. Move and Configure Redis-Server"
+echo "*****************************************"
+sudo mv redis-server /etc/init.d
+sudo chmod 755 /etc/init.d/redis-server
+echo "*****************************************"
+echo " 7. Auto-Enable Redis-Server"
+echo "*****************************************"
+sudo chkconfig --add redis-server
+sudo chkconfig --level 345 redis-server on
+echo "*****************************************"
+echo " 8. Start Redis Server"
+echo "*****************************************"
+sudo service redis-server start
+echo "*****************************************"
+echo " Complete!"
+echo " You can test your redis installation using the redis console:"
+echo "   $ src/redis-cli"
+echo "   redis> set foo bar"
+echo "   OK"
+echo "   redis> get foo"
+echo "   bar"
+echo "*****************************************"
+read -p "Press [Enter] to continue..."
+
